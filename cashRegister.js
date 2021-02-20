7cb6c5a8aaeceed7eeebeb51d4f2a007714e3903
idKrazu <idkrazu@gmail.com>
2021-02-21 00:06:48 +0100

    ✨ Cash Register finished

--- cashRegister.js
+++ cashRegister.js
@@ -1,73 +1,79 @@
 function checkCashRegister(price, cash, cid) {
-    let neededChange = cash - price
-    let returnObj = {
-      status: '',
-      change: []
-    }
-    let cashInDrawer = cid.reduce((a, b) => a + parseFloat(b[1]), 0)
-    cashInDrawer = parseFloat(cashInDrawer.toFixed(2))
-    if (cashInDrawer < neededChange) {
-      returnObj.status = 'INSUFFICIENT_FUNDS'
-    } else if (cashInDrawer === neededChange) {
-      returnObj.status = 'CLOSED'
-      returnObj.change = cid
-    } else if (calculateChange(neededChange, cid) === []) { 
-      returnObj.status = 'INSUFFICIENT_FUNDS'
-    } else {
-      returnObj.status = 'OPEN'
-      returnObj.change = calculateChange(neededChange, cid)
-    }
-  
-   
-    
-    return returnObj; 
+  let neededChange = cash - price;
+  let returnObj = {
+    status: "",
+    change: [],
+  };
+
+  // Amount of money in drawer
+  let cashInDrawer = cid.reduce((a, b) => a + parseFloat(b[1]), 0);
+  cashInDrawer = parseFloat(cashInDrawer.toFixed(2));
+
+  if (cashInDrawer < neededChange) {
+    // Not enough money
+    returnObj.status = "INSUFFICIENT_FUNDS";
+  } else if (cashInDrawer === neededChange) {
+    // All the money from drawer
+    returnObj.status = "CLOSED";
+    returnObj.change = cid;
+  } else if (calculateChange(neededChange, cid).length > 0) {
+    // Can give change
+    returnObj.status = "OPEN";
+    returnObj.change = calculateChange(neededChange, cid);
+  } else {
+    // Cannot return exact change
+    returnObj.status = "INSUFFICIENT_FUNDS";
   }
-  
-  function calculateChange(change, cid) {
-    let arr = []
-    const unitValue = {
-      'PENNY': 0.01,
-      'NICKEL': 0.05,
-      'DIME': 0.1,
-      'QUARTER': 0.25,
-      'ONE': 1,
-      'FIVE': 5,
-      'TEN': 10,
-      'TWENTY': 20,
-      'ONE HUNDRED': 100
-    }
-    let cidArr = [...cid]
-    cidArr.reverse()
-    for (const element of cidArr) {
-      // console.log(element[1], unitValue[element[0]])
-        let valueOfCurrentUnit = unitValue[element[0]]
-        let sumValueOfCurrentUnit = element[1]
-      
-      if (valueOfCurrentUnit <= change && sumValueOfCurrentUnit != 0) {
-        
-        let unitArr = [element[0], 0]
-        // console.log(change, unitValue[element[0]])
-        while (valueOfCurrentUnit <= change && sumValueOfCurrentUnit != 0) {
-           change -= valueOfCurrentUnit
-           change = parseFloat(change.toFixed(2))
-           console.log({change})
-           sumValueOfCurrentUnit -= valueOfCurrentUnit
-           unitArr[1] += valueOfCurrentUnit 
-        }
-        arr.push(unitArr)
-        
+
+  return returnObj;
+}
+
+function calculateChange(change, cid) {
+  let arr = [];
+  const unitValue = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
+  };
+
+  // Copy and reverse change array to start from biggest value
+  let cidArr = [...cid];
+  cidArr.reverse();
+
+  for (const element of cidArr) {
+    let valueOfCurrentUnit = unitValue[element[0]];
+    let sumValueOfCurrentUnit = element[1];
+
+    if (valueOfCurrentUnit <= change && sumValueOfCurrentUnit != 0) {
+      // [unitName, amountTakenFromDrawer]
+      let unitArr = [element[0], 0];
+
+      while (valueOfCurrentUnit <= change && sumValueOfCurrentUnit != 0) {
+        // Substract money from change
+        change -= valueOfCurrentUnit;
+        change = parseFloat(change.toFixed(2));
+
+        // Keep track how much money left in drawer
+        sumValueOfCurrentUnit -= valueOfCurrentUnit;
+
+        // Keep track how much money was taken
+        unitArr[1] += valueOfCurrentUnit;
       }
-      
-        
+      arr.push(unitArr);
     }
-    let availableChange = arr.reduce((a, b) => a + parseFloat(b[1]), 0)
-    if (availableChange < change) {
-      arr = []
-    }
-    
-    return arr
   }
-  
-  
-  
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+
+  // Check if it is enough money
+  let availableChange = arr.reduce((a, b) => a + parseFloat(b[1]), 0);
+  if (availableChange < change) {
+    arr = [];
+  }
+
+  return arr;
+}
